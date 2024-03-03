46cc2c79e5dbe2dc32f8570f1ab7b75a75f3ab0a
YuyaItabashi3594 <asuholover@gmail.com>
2024-03-04 02:18:58 +0900

    Zennリンクまで実装

---
+++ components/ArticleCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useDateFormat } from '@vueuse/core'
+
+const props = defineProps({
+  article: Object
+})
+
+console.log(props.article)
+
+</script>
+
+<template>
+  <NuxtLink :to="`https://zenn.dev${props.article.path}`">
+  <div class="flex bg-zinc-800 items-center justify-start divide-x">
+    <p class="text-xl flex-none w-6 text-center">{{ props.article.emoji }}</p>
+    <div class="flex flex-col ml-2 pl-1">
+      <p class="text-sm">{{ useDateFormat(props.article.published_at,'YYYY年MM月DD日') }}</p>
+      <p class="text-lg">{{ props.article.title }}</p>
+    </div>
+  </div>
+  </NuxtLink>
+
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <script setup>
+const { data, pending, error, refresh } = await useFetch('https://zenn.dev/api/articles?username=nemunyan')
 
-const {data, pending, error, refresh} = await useFetch('https://zenn.dev/api/articles?username=nemunyan')
+const articles = data.value.articles.slice(0, 3)
 
-const articles = data.value.articles
+console.log(articles)
 
 </script>
 
@@ -13,7 +14,7 @@ const articles = data.value.articles
   </div>
   <div class="flex flex-col items-center justify-center">
     <div class="border border-sky-600 p-2 rounded-lg bg-zinc-900 mt-4">
-      <p class="text-center text-lg">得意な言語/FW</p>
+      <p class="text-center text-lg">得意なもの</p>
       <div class="flex mt-4 gap-2">
         <Logo :language="'vue'" :url="'https://ja.vuejs.org/'" />
         <Logo :language="'nuxtjs'" :url="'https://nuxt.com/'" />
@@ -23,13 +24,24 @@ const articles = data.value.articles
       </div>
     </div>
     <div class="border border-sky-600 p-2 rounded-lg bg-zinc-900 mt-4">
-      <p class="text-center text-lg">学習中の言語/FW</p>
+      <p class="text-center text-lg">学習中のもの</p>
       <div class="flex mt-4 gap-2">
         <Logo :language="'react'" :url="'https://ja.react.dev/'" />
         <Logo :language="'nextjs'" :url="'https://nextjs.org/'" />
         <Logo :language="'python'" :url="'https://www.python.org/'" />
         <Logo :language="'django'" :url="'https://www.djangoproject.com/'" />
         <Logo :language="'laravel'" :url="'https://laravel.com/'" />
+      </div>
+    </div>
+    <div class="flex flex-col md:flex-row mt-6">
+      <div class="border rounded-md mx-4 bg-zinc-900">
+        <div class="flex items-center px-2 pt-2">
+          <img src="/zenn.png" class="w-6 h-6">
+          <p class="text-lg">最新投稿</p>
+        </div>
+        <div v-for="article in articles" :key="article.id" class="w-full md:w-1/3 mt-4 ">
+          <ArticleCard :article="article" />
+        </div>
       </div>
     </div>
   </div>
