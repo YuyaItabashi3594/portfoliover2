efded6d415a03c97b43ee6e6d862faeda8159f16
YuyaItabashi3594 <asuholover@gmail.com>
2024-03-20 23:54:28 +0900

    History作成

--- components/HistoryCard.vue
+++ components/HistoryCard.vue
@@ -7,12 +7,12 @@ const props = defineProps({
 
 <template>
   <ol class="shadow-md max-w-2xl mx-2">
-    <li class="flex items-center justify-center ms-2 mb-10">
+    <li class="flex items-center justify-start ms-2 mb-4 md:mb-10">
       <div class="shrink-0">
         <img :src="props.data.img" class="w-32 p-2">
       </div>
       <div class="p-6">
-        <p class="text-2xl leading-tight font-medium">{{ props.data.title }}</p>
+        <p class="text-xl leading-tight font-medium">{{ props.data.title }}</p>
         <p class="mt-4 text-zinc-400">{{ props.data.description }}</p>
       </div>
     </li>
--- pages/about.vue
+++ pages/about.vue
@@ -2,34 +2,50 @@
 
 const histories = [
   {
-    date:'1996年',
+    date: '1996年',
     img: '/about/born.png',
     title: '誕生',
     description: '茨城県つくば市で生まれました。泣かなかったそうです。'
   },
   {
-    date:'2015年',
+    date: '2015年',
     img: '/about/clark.jpg',
     title: 'クラーク記念国際高等学校卒業',
     description: 'クラーク全国初の東北大学合格でした。'
+  },
+  {
+    date: '2022年',
+    img: '/about/tohokuuniversity.jpg',
+    title: '東北大学経済学研究科修士課程修了',
+    description: '専攻は経営組織論です。'
+  },
+  {
+    date: '現在',
+    img: '',
+    title: 'フロントエンドエンジニア',
+    description: '3社ほどを通じて働いてきました。これまでの経験から，次はサービスに責任をもって取り組める仕事に就けるように努力しています。'
   }
 ]
 
 </script>
 
 <template>
-  <div class="flex items-center justify-center">
-    <div class="text-2xl">History</div>
-  </div>
-  <div class="flex items-center justify-center px-4 mt-4">
-    <div class="flex flex-col">
-      <div class="relative border-s border-zinc-700" v-for="data in histories">
-        <div class="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-zinc-900 bg-zinc-700"></div>
-        <time class="ml-4 mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{{ data.date }}</time>
-        <HistoryCard :data="data" />
+  <div class="flex flex-col md:flex-row items-center justify-center">
+    <div class="flex flex-col items-center justify-center px-4 mt-4">
+      <div class="text-2xl">History</div>
+      <div class="flex flex-col">
+        <div class="relative border-s border-zinc-700" v-for="data in histories">
+          <div class="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-zinc-900 bg-zinc-700"></div>
+          <time class="ml-4 mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{{ data.date
+            }}</time>
+          <HistoryCard :data="data" />
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col items-center justify-center px-4 mt-4">
+      <div class="text-2xl">Favorite Things</div>
+      <div class="flex flex-col">
       </div>
     </div>
   </div>
-
-
 </template>
